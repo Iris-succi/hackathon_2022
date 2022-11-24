adb22389ff154cb1500c5f7c9e841789bbf21b08
Chloé Bidau <chloebidau@hotmail.fr>
2022-11-24 23:22:55 +0100

    creation and css of loader

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
+import "../style/Loader.css";
+import Lottie from "react-lottie";
+import animationData from "../lotties/Loader.json";
 
 function Loader() {
-  return <div>Loader</div>;
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  return (
+    <div className="">
+      <Lottie options={defaultOptions} height={700} width={700} />
+      <NavLink to="/">
+        <span className="loaderText text-5xl font-bold">Wait for it...</span>
+      </NavLink>
+    </div>
+  );
 }
 
 export default Loader;
