be255e4869d9dfb8954c539e746faa5d646f307f
IRIS/SUCCIE <[succi.iris@gmail.com](mailto:succi.iris@gmail.com)]>
2022-11-25 00:20:07 +0100

    add favorites ok

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,7 +1,29 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-array-index-key */
 import React from "react";
+import FavoriteCard from "../components/FavoriteCard";
 
 function Favorites() {
-  return <div>Favorites</div>;
+  /* Use state for stock data in the localstorage */
+  const [displayCardTrip, setDisplayCardTrip] = React.useState();
+  // const [displayCardHotels, setDisplayCardHotels] = React.useState();
+  /* use effect for get the data in the localstorage */
+  React.useEffect(() => {
+    const cardData = localStorage.getItem("favorite");
+    setDisplayCardTrip(JSON.parse(cardData));
+  }, []);
+  const datas = JSON.parse(localStorage.getItem("favorites"));
+
+  return (
+    <div className="flex justify-center align-center">
+      <div className="flex-col text-center">
+        <h1 className="text-3xl mt-5 mb-5">My favorites recipes</h1>
+        {datas.map((data) => (
+          <FavoriteCard key={data.id} data={data} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Favorites;
