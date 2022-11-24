3bc0a6f30dae72f2fcec809fef8bb781371146c5
Chloé Bidau <chloebidau@hotmail.fr>
2022-11-25 00:09:17 +0100

    beginning the about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import "../style/About.css";
+import AboutUs from "../images/AboutUs.jpg";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div className="border">
+      <div className="h-36">
+        <img className="border w-97" alt="aboutus" src={AboutUs} />
+      </div>
+      <div>
+        <p>
+          "Les petits nems" is a french team of coder who like not to slepp and
+          to eat pizza!
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
