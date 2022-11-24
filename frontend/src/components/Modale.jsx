397a13d26abc264e71bd717086ddb525178ae45b
Marion Lalonde <lalondemarion.perso@gmail.com>
2022-11-24 23:47:15 +0100

    Adding modale changes

--- frontend/src/components/Modale.jsx
+++ frontend/src/components/Modale.jsx
@@ -15,8 +15,8 @@ export default function Modal({ showModal, setShowModal, randomizer }) {
       </button>
       {showModal ? (
         <div className="modal">
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="h-100 relative w-auto my-6 mx-auto max-w-3xl bg-bluetitle">
+          <div className="rounded justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="shadow-lg h-100 relative w-auto my-6 mx-auto max-w-3xl bg-bluetitle rnded-modale">
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 <div className=" text-backgroundMain backgroundMain text-center justify-between pt-5 border-b border-solid border-slate-200 rounded-t">
                   <h3 className="text-3xl font-semibold text-center">
@@ -31,14 +31,30 @@ export default function Modal({ showModal, setShowModal, randomizer }) {
                     </span>
                   </button>
                 </div>
-                <div className="relative p-6 flex-auto text-backgroundMain">
-                  <img src={randomizer[0].strMealThumb} alt="recipe" />
-                  <div className="mt-5">Country : {randomizer[0].strArea}</div>
-                  <div className="mt-5">
-                    Categorie : {randomizer[0].strCategory}
+                <div className="relative p-6 flex flex-col text-backgroundMain w-screen">
+                  <div
+                    style={{
+                      backgroundImage: `url('${randomizer[0].strMealThumb}')`,
+                    }}
+                    className="img-modale   rounded shadow-lg"
+                  />
+                  <div className="flex ">
+                    <div className="text-left p-2  mt-5">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="17"
+                        height="17"
+                        fill="#A6122D"
+                        className="bi bi-airplane-fill inline-block ml-2 transform rotate-45"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M6.428 1.151C6.708.591 7.213 0 8 0s1.292.592 1.572 1.151C9.861 1.73 10 2.431 10 3v3.691l5.17 2.585a1.5 1.5 0 0 1 .83 1.342V12a.5.5 0 0 1-.582.493l-5.507-.918-.375 2.253 1.318 1.318A.5.5 0 0 1 10.5 16h-5a.5.5 0 0 1-.354-.854l1.319-1.318-.376-2.253-5.507.918A.5.5 0 0 1 0 12v-1.382a1.5 1.5 0 0 1 .83-1.342L6 6.691V3c0-.568.14-1.271.428-1.849Z" />
+                      </svg>{" "}
+                      {randomizer[0].strArea} - {randomizer[0].strCategory}
+                    </div>
                   </div>
                   <a href={randomizer[0].strYoutube}>
-                    <img src={youtube} alt="" className="w-12" />
+                    <img src={youtube} alt="" className="w-12 m-1 p-1" />
                   </a>
                 </div>
                 <div className="flex text-backgroundMain items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
